447213a537e3ba9d66eeb0df7d08bd09d9711bfe
Abhinendra <abhicse10001@gmail.com>
2023-09-05 19:04:25 +0530

    Added highlight boxes(to show the current row, column and 3x3 square) feature

--- src/suduko/Board.java
+++ src/suduko/Board.java
@@ -8,6 +8,7 @@ import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -35,6 +36,7 @@ public class Board extends JFrame implements ActionListener, FocusListener {
     JButton one, two, three, four, five, six, seven, eight, nine, erase, check, newGame, getSolution;
     JTextField currentBlock;
     int difficultyCount;
+    boolean[][] flag = new boolean[9][9]; // to identify every textfield 
 
     JTextField[][] blockNo = new JTextField[9][9];
     int[][] puzzle;
@@ -98,13 +100,13 @@ public class Board extends JFrame implements ActionListener, FocusListener {
         }
 
         JLabel difficultylabel = new JLabel("Choose Difficulty Level");
-        difficultylabel.setBounds(30, 30, 250, 30);
+        difficultylabel.setBounds(45, 30, 250, 30);
         difficultylabel.setFont(new Font("Tahoma", Font.PLAIN, 23));
         add(difficultylabel);
 
         difficulty = new Choice();
-        difficulty.setBounds(300, 30, 100, 30);
-        difficulty.setFont(new Font("Tahoma", Font.PLAIN, 23));
+        difficulty.setBounds(320, 30, 120, 30);
+        difficulty.setFont(new Font("Railway", Font.PLAIN, 23));
         difficulty.setBackground(Color.WHITE);
         difficulty.add("Easy");
         difficulty.add("Medium");
@@ -255,36 +257,43 @@ public class Board extends JFrame implements ActionListener, FocusListener {
             } else {
                 JOptionPane.showMessageDialog(null, "Invalid");
             }
-            printSudoku(puzzle);
+            //printSudoku(puzzle);
         } else if (ae.getSource() == newGame) {
+
+            //this for loop is created to assign enabled textfield with true and disabled with false(by default)
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    flag[i][j] = true;
+                }
+            }
             puzzle = Puzzle.getPuzzle();
-            fillBox(puzzle);
+            fillBox(puzzle);;
         } else if (ae.getSource() == getSolution) {
             fillSolution(puzzle);
-            printSudoku(puzzle);
+            //printSudoku(puzzle);
         }
     }
 
-    public void printSudoku(int[][] puzzle) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(puzzle[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
+    //for testing purpose only
+//    public void printSudoku(int[][] puzzle) {
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                System.out.print(puzzle[i][j] + " ");
+//            }
+//        }
+//    }
     public void fillSolution(int[][] board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (!blockNo[i][j].getBackground().equals(new Color(173, 216, 230))) {
+                if (flag[i][j]) {
                     blockNo[i][j].setText(board[i][j] + "");
+                    blockNo[i][j].setForeground(new Color(20, 180, 5));
                 }
             }
         }
     }
 
+    //to choose visible number randomly
     public JTextField randomBox() {
         Random rand = new Random();
         int i = rand.nextInt(9);
@@ -295,14 +304,17 @@ public class Board extends JFrame implements ActionListener, FocusListener {
 
     public void fillBox(int[][] puzzle) {
 
-        if((difficulty.getSelectedItem()).equals("Easy")){
+        if ((difficulty.getSelectedItem()).equals("Easy")) {
             difficultyCount = 50;
-        }else if((difficulty.getSelectedItem()).equals("Medium")){
+        } else if ((difficulty.getSelectedItem()).equals("Medium")) {
             difficultyCount = 42;
-        }else{
+        } else {
             difficultyCount = 35;
         }
+
         int count = 0;
+
+        //to remove the problem of overlapping of box background colors
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 blockNo[i][j].setBackground(Color.white);
@@ -316,7 +328,7 @@ public class Board extends JFrame implements ActionListener, FocusListener {
                 if (count < difficultyCount) {
                     JTextField box = new JTextField();
                     box = randomBox();
-                    box.setBackground(new Color(173, 216, 230));
+                    box.setBackground(new Color(94, 181, 206));
                     box.setDisabledTextColor(Color.black);
                     box.setEnabled(false);
                     box.setEditable(false);
@@ -325,23 +337,36 @@ public class Board extends JFrame implements ActionListener, FocusListener {
             }
         }
 
+        //assign flag to every jtextfield to indentify
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (!blockNo[i][j].getBackground().equals(new Color(173, 216, 230))) {
+                if (blockNo[i][j].getBackground().equals(new Color(94, 181, 206))) {
+                    flag[i][j] = false;
+                }
+            }
+        }
+
+        //for testing purpose
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                System.out.print(flag[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (flag[i][j]) {
                     blockNo[i][j].setText("");
                     blockNo[i][j].setFont(new Font("Tahoma", Font.PLAIN, 25));
-                    blockNo[i][j].setForeground(new Color(20, 180, 5));
+                    blockNo[i][j].setForeground(Color.white);
                     blockNo[i][j].setEditable(true);
                     blockNo[i][j].setEnabled(true);
                 }
             }
         }
-        printSudoku(puzzle);
-        System.out.println(SudokuValidator.isValidSudoku(puzzle));
-    }
 
-    public static void main(String[] args) {
-        new Board();
+//        printSudoku(puzzle);
+        //System.out.println(SudokuValidator.isValidSudoku(puzzle)); //to check if the generated sudoku is valid or not
     }
 
     public boolean checkSolution(int[][] sol) {
@@ -365,10 +390,206 @@ public class Board extends JFrame implements ActionListener, FocusListener {
     @Override
     public void focusGained(FocusEvent fe) {
         currentBlock = (JTextField) fe.getSource();
+        currentBlock.setBackground(new Color(212, 235, 242));
+
+        Rectangle currentbox = new Rectangle();
+        currentbox = currentBlock.getBounds();
+        int X = currentbox.x;
+        int Y = currentbox.y;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                Rectangle box = new Rectangle();
+                box = blockNo[i][j].getBounds();
+
+                if (box.x == X || box.y == Y) { // to highlight the row and the coloum of selected box
+                    blockNo[i][j].setBackground(new Color(212, 235, 242));//lighter blue
+                    System.out.println(X + " " + Y);
+
+                    if ((X == 0 || X == 55 || X == 110) && (Y == 0 || Y == 55 || Y == 110)) {
+                        
+                        for(int s = 0; s <3; s++){
+                            for(int t = 0; t<3; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[0][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[0][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[0][2].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][2].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][2].setBackground(new Color(212, 235, 242));
+                    }else if ((X == 0 || X == 55 || X == 110) && (Y == 170 || Y == 225 || Y == 280)) {
+                        
+                        for(int s = 3; s <6; s++){
+                            for(int t = 0; t<3; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[3][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[3][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[3][2].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][2].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][2].setBackground(new Color(212, 235, 242));
+                    }else if ((X == 0 || X == 55 || X == 110) && (Y == 340 || Y == 395 || Y == 450)) {
+                        
+                        for(int s = 6; s <9; s++){
+                            for(int t = 0; t<3; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[6][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[6][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[6][2].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][2].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][0].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][1].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][2].setBackground(new Color(212, 235, 242));
+                        
+                    }else if ((X == 170 || X == 225 || X == 280) && (Y == 0 || Y == 55 || Y == 110)) {
+                        
+                        for(int s = 0; s <3; s++){
+                            for(int t = 3; t<6; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[0][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[0][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[0][5].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][5].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][5].setBackground(new Color(212, 235, 242));
+                    }else if ((X == 170 || X == 225 || X == 280) && (Y == 170 || Y == 225 || Y == 280)) {
+                        
+                        for(int s = 3; s <6; s++){
+                            for(int t = 3; t<6; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[3][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[3][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[3][5].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][5].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][5].setBackground(new Color(212, 235, 242));
+                    }else if ((X == 170 || X == 225 || X == 280) && (Y == 340 || Y == 395 || Y == 450)) {
+                        
+                        for(int s = 6; s <9; s++){
+                            for(int t = 3; t<6; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[6][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[6][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[6][5].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][5].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][3].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][4].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][5].setBackground(new Color(212, 235, 242));
+                        
+                    }else if ((X == 340 || X == 395 || X == 450) && (Y == 0 || Y == 55 || Y == 110)) {
+                        
+                        for(int s = 0; s <3; s++){
+                            for(int t = 6; t<9; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[0][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[0][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[0][8].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[1][8].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[2][8].setBackground(new Color(212, 235, 242));
+                    }else if ((X == 340 || X == 395 || X == 450) && (Y == 170 || Y == 225 || Y == 280)) {
+                        
+                        for(int s = 3; s <6; s++){
+                            for(int t = 6; t<9; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[3][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[3][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[3][8].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[4][8].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[5][8].setBackground(new Color(212, 235, 242));
+                    }else if ((X == 340 || X == 395 || X == 450) && (Y == 340 || Y == 395 || Y == 450)) {
+                        
+                        for(int s = 6; s <9; s++){
+                            for(int t = 6; t<9; t++){
+                                blockNo[s][t].setBackground(new Color(212, 235, 242));
+                            }
+                        }
+//                        blockNo[6][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[6][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[6][8].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[7][8].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][6].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][7].setBackground(new Color(212, 235, 242));
+//                        blockNo[8][8].setBackground(new Color(212, 235, 242));
+                    }
+
+                }
+            }
+        }
     }
 
     @Override
     public void focusLost(FocusEvent fe) {
+        currentBlock.setBackground(Color.white);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                blockNo[i][j].setBackground(Color.white);
+            }
+        }
+
+        Rectangle currentbox = new Rectangle();
+        currentbox = currentBlock.getBounds();
+        int X = currentbox.x;
+        int Y = currentbox.y;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                Rectangle box = new Rectangle();
+                box = blockNo[i][j].getBounds();
+
+                if ((box.x == X || box.y == Y) && flag[i][j] == false) {
+                    blockNo[i][j].setBackground(Color.white);
+                }
+
+                if (!flag[i][j]) {
+                    blockNo[i][j].setBackground(new Color(94, 181, 206));//darker blue
+                }
+            }
+        }
         currentBlock = null;
+    }
+
+    public static void main(String[] args) {
+        new Board();
     }
 }
